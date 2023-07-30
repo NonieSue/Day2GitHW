7e867938fac368d0b74fc61b4375598635be11eb
Winona <Winona@Winona-Dell>
2023-07-30 08:22:18 -0400

    rearranged code

--- src/hw/PushPullStash.java
+++ src/hw/PushPullStash.java
@@ -1,17 +1,19 @@
 package hw;
 
 public class PushPullStash {
-	
+
 	public static void main(String[] args) {
-		
-		int a = 2;
-		int b = 2;
-		
+
 		System.out.println("Winona is awesome");
+
+		int a = 5;
+		int b = 2;
+
 	}
+
 	public static void getInfo() {
 		System.out.println("Info");
+
 	}
-	
 
 }
