c28f5043338b8d595e88c33c902da01a371014a0
Winona <Winona@Winona-Dell>
2023-07-30 08:13:16 -0400

    added variables

--- src/hw/PushPullStash.java
+++ src/hw/PushPullStash.java
@@ -4,6 +4,9 @@ public class PushPullStash {
 	
 	public static void main(String[] args) {
 		
+		int a = 2;
+		int b = 2;
+		
 		System.out.println("Winona is awesome");
 	}
 	public static void getInfo() {
